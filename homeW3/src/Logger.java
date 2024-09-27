42a5ce680a5b685837d71fe8ba62cb887665a598
1nutilis <145498715+1nutilis@users.noreply.github.com>
2024-09-27 17:51:19 +0500

    Add files via upload

---
+++ homeW3/src/Logger.java
@@ -0,0 +1,20 @@
+//DRY
+
+
+public class Logger {
+    public void log(String message, String level) {
+        System.out.println(level + ": " + message);
+    }
+
+    public void logError(String message) {
+        log(message, "ERROR");
+    }
+
+    public void logWarning(String message) {
+        log(message, "WARNING");
+    }
+
+    public void logInfo(String message) {
+        log(message, "INFO");
+    }
+}
---
+++ homeW3/src/User.java
@@ -0,0 +1,14 @@
+//YAGNI
+
+
+public class User {
+    private String name;
+    private String email;
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    // Геттеры и сеттеры
+}
